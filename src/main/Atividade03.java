85fe0b635c3885991432d251c87ab008795f89db
ApenasOLinco <lincoln.matos1003@gmail.com>
2025-02-24 15:53:29 -0300

    ADD Finalizada atv3

--- src/main/Atividade03.java
+++ src/main/Atividade03.java
@@ -11,8 +11,32 @@ import java.util.Scanner;
 
 public class Atividade03 {
     public static void main(String[] args) {
-        int latas, garrafas600Ml, garrafas2L;
+        float latas350Ml, garrafas600Ml, garrafas2L;
         float litros;
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Insira o número de latas de 350ml compradas: ");
+        latas350Ml = scanner.nextInt();
+        
+        System.out.println("Insira o número de garrafas de 600ml compradas: ");
+        garrafas600Ml = scanner.nextInt();
+        
+        System.out.println("Insira o número de garrafas de 2L compradas: ");
+        garrafas2L = scanner.nextInt();
+        
+        litros = latas350Ml * .35f + garrafas600Ml * .6f + garrafas2L * 2f;
+        
+        scanner.close();
+        
+        System.out.println("Litros comprados: " + litros);
     }
 }
+
+
+
+
+
+
+
+
+
